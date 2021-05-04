49852a1b53bb30093f5b38bb73a0da83e297c66c
anyhont <82115631+anyhont@users.noreply.github.com>
2021-05-04 17:55:48 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -4,3 +4,15 @@ function preload() {
   mySound = loadSound(mysterious.mp3);
 }
 
+function setup() {
+  let cnv = createCanvas(300, 300);
+  cnv.mousePressed(canvasPressed);
+  background(345);
+  text('tap here to play', 40, 50);
+}
+
+function canvasPressed() {
+  // playing a sound file on a user gesture
+  // is equivalent to `userStartAudio()`
+  mySound.play();
+}
