c296aa0ac1700f49078a4bc51b3aeddd977ee356
anyhont <82115631+anyhont@users.noreply.github.com>
2021-05-05 16:15:11 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,14 +1,7 @@
-var song;
-
-function preload(){
-song = loadSound("mysterious.mp3");
+function setup() {
+  createCanvas(400, 400);
 }
 
-function setup(){
-createCanvas(500, 500);
-song.play();
-}
-
-function draw(){
-background(0);
+function draw() {
+  background(220);
 }
